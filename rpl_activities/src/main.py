8cc5b02d8a2012e55615cd2c6bb60c2b86bb08d1
Alejandro Levinas <levinasale@gmail.com>
2026-02-28 16:48:41 +0000

    Setting up Ona and Agents.md

--- rpl_activities/src/main.py
+++ rpl_activities/src/main.py
@@ -16,6 +16,7 @@ app = FastAPI(lifespan=users_api_conn_lifespan, **FASTAPI_METADATA)
 
 
 app.add_middleware(
+    # CORSMiddleware, allow_origins=["http://localhost:3000","http://localhost:8088"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
     CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
 )
 
